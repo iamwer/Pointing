ad43c8b0d562f7283a5bc40063183e01cdf067a7
alexandr shishigin <iamwer4@gmail.com>
2022-08-09 12:08:50 +0900

    Добавьте файлы проекта.

---
+++ Pointing/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	int* pAPointer;
+	int* pScore = 0;
+	int score = 100;
+	pScore = &score;
+	cout << "Assigning &score to pScore\n";
+	cout << "&score is: " << &score << "\n";
+	cout << "pScore is " << pScore << "\n";
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+
+	cout << "Adding 500 to score\n";
+	score += 500;
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+	cout << "Adding 500 to *pScore\n";
+	*pScore += 500;
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+	cout << "Assigning &newScore to pScore\n";
+	int newScore = 5000;
+	pScore = &newScore;
+	cout << "&newScore is: " << &newScore << "\n";
+	cout << "pScore is: " << pScore << "\n";
+	cout << "newScore is: " << newScore << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+	cout << "Assigning &str to pStr\n";
+	string str = "score";
+	string* pStr = &str;
+	cout << "str is: " << str << "\n";
+	cout << "*pStr is: " << *pStr << "\n";
+	cout << "(*pStr).size() is: " << (*pStr).size() << "\n";
+	cout << "pStr->size() is: " << pStr->size() << "\n";
+	return 0;
+
+}
